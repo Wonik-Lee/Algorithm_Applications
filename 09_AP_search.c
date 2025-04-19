66e7ea40995e59450a836e21139f6c792e450162
Wonik-Lee <lwnick@naver.com>
2025-04-19 13:22:04 +0900

    Update 09_AP_search.c

--- 09_AP_search.c
+++ 09_AP_search.c
@@ -60,19 +60,20 @@ int AP_recur(node* a[], int i)
     int min; // 현재 정점(i)를 루트로 하는 서브트리 전체가 올라갈 수 있는 가장 작은 조상노드
     int m;
     check[i] = min = ++order;  // 방문 순번을 저장하는 배열
-    for (t = a[i]; t != NULL; t = t->next) {
+
+    for (t = a[i]; t != NULL; t = t->next) { 
         if (i == 0 && check[t->vertex] == 0)
-            son_of_root++;   //루트에서 내려가는 분기 수를 세는 역할
+            son_of_root++;   //루트에서 내려가는 분기 수를 세는 역할(son_of_root가 2 이상이면 루트도 단절점)
         if (check[t->vertex] == 0) {
-            m = AP_recur(a, t->vertex);
-            if (m < min) min = m;
-            if (m >= check[i] && i != 0)
+            m = AP_recur(a, t->vertex);  // 자식들이 올라갈 수 있는 제일 상위 노드를 가져옴.
+            if (m < min) min = m;        // m < min 이면 m이 더 상위노드니까, min을 m으로 업데이트.
+            if (m >= check[i] && i != 0) // 현재 루트노드가 아니고, 자식들이 올라갈 수 있는 제일 상위 노드 m이 나(check[i])보다 낮다면, 단절점
                 printf("* %c %2d : %d\n", int2name(i), check[i], m);   // 단절점 표시
             else
                 printf("  %c %2d : %d\n", int2name(i), check[i], m);   // 단절점 아님
         }
-        else
-            if (check[t->vertex] < min)
+        else // 이미 방문했던 노드라면 
+            if (check[t->vertex] < min) // 그 노드가 min보다 더 상위 노드면 업데이트
                 min = check[t->vertex];
     }
     return min;
